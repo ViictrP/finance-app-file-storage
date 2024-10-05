0c92cfd0808c7e183d586925718393ff425e558a
Victor Prado <victor.prado@arc-one.com>
2024-10-05 13:32:24 -0300

    file upload

---
+++ financeapp-file-upload/src/main/java/com/victor/financeapp/file/upload/FinanceappFileUploadApplication.java
@@ -0,0 +1,13 @@
+package com.victor.financeapp.file.upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceappFileUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceappFileUploadApplication.class, args);
+	}
+
+}
