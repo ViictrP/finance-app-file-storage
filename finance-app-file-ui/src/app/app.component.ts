6f7a4318b223f76cb943bb8c7f0dd6fbc2041519
Victor Prado <victor.prado@arc-one.com>
2024-10-10 03:13:30 -0300

    Layout construction

--- finance-app-file-ui/src/app/app.component.ts
+++ finance-app-file-ui/src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DatePipe } from '@angular/common';
+
+declare var duoIcons: {
+    createIcons: Function;
+};
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+    imports: [RouterOutlet, DatePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'finance-app-file-ui';
+export class AppComponent implements OnInit {
+    title = 'Storage Management';
+    today = new Date();
+
+    ngOnInit(): void {
+        if (typeof duoIcons !== 'undefined') {
+            duoIcons.createIcons();
+        }
+    }
 }
